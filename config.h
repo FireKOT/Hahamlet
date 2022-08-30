bbcdcf627e70b173f614270eb49a8011118610e6
FireKOT <58856290+FireKOT@users.noreply.github.com>
2022-08-30 16:48:53 +0300

    Add files via upload

--- config.h
+++ config.h
@@ -9,12 +9,6 @@
 
 #define ERR_MSG(err_code) printf("Error code:%s in file: %s in function %s in line %d\n",\
                                       #err_code, __FILE__, __PRETTY_FUNCTION__, __LINE__)
-#else
-
-    #define ERR_MSG(err_code)
-
-#endif
-
 
 
 #endif
